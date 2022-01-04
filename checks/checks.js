22aa8cb1937bcc160bef688e47a1053578d569eb
marlloncsoares <marlonmcs@hotmail.com>
2022-01-04 00:11:47 -0300

    feat: adiciona função isEqual

--- checks/checks.js
+++ checks/checks.js
@@ -44,11 +44,21 @@ const isEmpty = (value = null) => Object.keys(value).length === 0;
  */
 const isEmail = (value = null) => /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
 
+
+/**
+ * Verifies if value one is equal value two.
+ * @param {*} valueOne - Value one to be verified.
+ * @param {*} valueTwo - Value two to be verified.
+ * @returns Returns true, if informed values equals, false returns otherwise.
+ */
+const isEqual = (valueOne = null, valueTwo = null) => Object.is(valueOne, valueTwo);
+
 module.exports = {
     isNull,
     isNullish,
     isInt,
     isFloat,
     isEmpty,
-    isEmail
+    isEmail,
+    isEqual
 };
