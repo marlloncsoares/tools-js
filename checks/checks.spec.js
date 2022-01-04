d05860c82ef8dbd0e5a3c8c293bceb4d91c116bc
marlloncsoares <marlonmcs@hotmail.com>
2022-01-03 22:57:47 -0300

    feat: adiciona teste para método isFloat

--- checks/checks.spec.js
+++ checks/checks.spec.js
@@ -1,4 +1,4 @@
-const checks = require('./checks').default;
+const checks = require('./checks');
 
 describe('Checks', () => {
     test('should return true if a null value is provided.', () => {
@@ -20,6 +20,16 @@ describe('Checks', () => {
     test('should return true if a integer value is provided.', () => {
         const value = 5;
         expect(checks.isInt(value)).toBe(true);
+    });
+
+    test('should return true if a float value is provided.', () => {
+        const value = 5.5;
+        expect(checks.isFloat(value)).toBe(true);
+    })
+
+    test('should return false if a not float value is provided.', () => {
+        const value = 5;
+        expect(checks.isFloat(value)).toBe(false);
     });
 
     test('should return true if a empty value is provided.', () => {
