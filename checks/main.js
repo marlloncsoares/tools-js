aa805b28fc0cb2073814ce48026c86d7b2eb5a0c
marlloncsoares <marlonmcs@hotmail.com>
2022-01-03 22:45:50 -0300

    feat: adiciona utilização da função isEmpty

--- checks/main.js
+++ checks/main.js
@@ -5,4 +5,5 @@ console.log('value: ', value);
 console.log('isNull: ', checks.isNull(value));
 console.log('isNullish: ', checks.isNullish(value));
 console.log('isInt: ', checks.isInt(value));
+console.log('isEmpty: ', checks.isEmpty(value));
 console.log('isEmail: ', checks.isEmail(value));
